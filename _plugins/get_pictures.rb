5fcb95ba5c1de3eedc8bc78c03d1e59bd9230828
jacobj10 <jacobj10@bbhcsd.org>
2017-02-14 00:39:35 -0500

    Fixed about rendering to adjust for blanks in the form

--- _plugins/get_pictures.rb
+++ _plugins/get_pictures.rb
@@ -1,5 +1,7 @@
 require "open-uri"
 
+PLACEHOLDER = "https://placeholdit.imgix.net/~text?txtsize=28&txt=300%C3%97300&w=300&h=300"
+
 Jekyll::Hooks.register :site, :after_init do |get_pictures|
     FileUtils.rm_rf(Dir.glob('./static/images/members/*'))
     CSV.foreach("./_data/members.csv") do |row|
@@ -7,13 +9,14 @@ Jekyll::Hooks.register :site, :after_init do |get_pictures|
             next
         end
         if row[4] == nil or row[4].include? 'linkedin'
-            row[4] = "https://placeholdit.imgix.net/~text?txtsize=28&txt=300%C3%97300&w=300&h=300"
+            row[4] = PLACEHOLDER
         end
         puts row[4]
         open(row[4]) {|f|
-           File.open("./static/images/members/" + row[1] + ".jpg","wb") do |file|
-             file.puts f.read
-           end
+            name = "./static/images/members/" + row[1]
+            File.open(name,"wb") do |file|
+                file.puts f.read
+            end
         }
     end
 end
