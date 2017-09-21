6da344a6124154f76d9d0ba4dcbe38b7b80bf535
Joshua Jacob <jacobj10@bbhcsd.org>
2017-09-21 16:56:20 -0400

    removed old plugin stuff, re-added static generation of data

--- precheck.rb
+++ precheck.rb
@@ -2,7 +2,7 @@ require "open-uri"
 require "csv"
 
 SRC_PATH = File.expand_path(File.dirname(__FILE__))
-CSV_FILE = "_members/members.csv"
+CSV_FILE = "_data/members.csv"
 PATH_TO_CSV = File.join SRC_PATH, CSV_FILE
 STATIC_DIR = "static/images/members/"
 WRITE_PATH = File.join SRC_PATH, STATIC_DIR 
@@ -14,7 +14,7 @@ def download_and_save(name, url)
 		file_path = File.join(WRITE_PATH, name)
 		File.open(file_path, "wb") do |file|
 			file.puts f.read
-		end			
+		end
 	}
 rescue
 	open(PLACEHOLDER) { |f|
