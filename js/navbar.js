058e2def46fd92d301a4f7b8762a4ff7046ed14c
Elton <eltonp3103@gmail.com>
2019-01-21 12:43:00 -0500

    removed old commented code

--- js/navbar.js
+++ js/navbar.js
@@ -7,13 +7,6 @@ const routeToId = {
 }
 
 const setCurrentPage = (pageUrl) => {
-  // get the path
-  // pass it to getElementById
-  // add the class to it
   const element = document.getElementById(routeToId[pageUrl]);
   element.classList.add("page-selected");
-  // REMVOVE THIS CODE
-  // var currentPage = document.getElementById('navabout');
-  // currentPage.style.fontWeight = 'bold';
-  // currentPage.style.color = 'white';
 }
